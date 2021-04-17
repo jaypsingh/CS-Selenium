449f7473c0878a24e29b0bcda533068ffeb3aaab
jaypsingh <jayprakash.singh3@yahoo.com>
2021-04-18 00:25:21 +0530

    added demohelper and added more tests to main file

--- CSSelenium/CSSeleniumDemo.cs
+++ CSSelenium/CSSeleniumDemo.cs
@@ -15,6 +15,8 @@ namespace CSSelenium
         string HomeUrl = "https://www.seleniumeasy.com/test/";
         string sampleAppUrl= "http://uitestingplayground.com/sampleapp";
         string uiTestPlayUrl = "http://uitestingplayground.com/";
+        string automationPracticeUrl = "http://automationpractice.com/index.php";
+        string basicCalculatorUrl = "https://testsheepnz.github.io/BasicCalculator.html";
         string PageTitle = "Selenium Easy - Best Demo website to practice Selenium Webdriver Online";
         
         [Fact]
@@ -151,6 +153,71 @@ namespace CSSelenium
 
                 //Assert
                 Assert.Equal(expectedPageTitle, actualPageTitle);
+            }
+        }
+
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void ClickOnElementByCSSSelector()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                //Arrange                
+                driver.Navigate().GoToUrl(automationPracticeUrl);
+                var expectedPageTitle = "Order - My Store";
+
+                //Act
+                IWebElement shoppingCartLink = driver.FindElement(By.CssSelector("[title='View my shopping cart']"));
+                DemoHelper.Pause(1000);
+                shoppingCartLink.Click();                
+
+                var actualPageTitle = driver.Title;
+
+                //Assert
+                Assert.Equal(expectedPageTitle, actualPageTitle);
+            }
+        }
+
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void ClickOnElementByClass()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                //Arrange                
+                driver.Navigate().GoToUrl(uiTestPlayUrl);
+                string expectedTitle = "UI Test Automation Playground";
+
+                //Act
+                // Note that class name should not contain space else this will not work.
+                IWebElement branLink = driver.FindElement(By.ClassName("navbar-brand"));
+                DemoHelper.Pause(1000);
+                branLink.Click();
+
+                var actualPageTitle = driver.Title;
+
+                //Assert
+                Assert.Equal(expectedTitle, actualPageTitle);
+            }
+        }
+
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void FindElementByTag()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                //Arrange                
+                driver.Navigate().GoToUrl(basicCalculatorUrl);
+                string expectedLabelText = "Build";
+
+                //Act
+                // Note that class if multiple tags are present on the page this will return the first tag found.
+                IWebElement labelElement = driver.FindElement(By.TagName("label"));
+                var actualLabelText = labelElement.Text;                
+
+                //Assert
+                Assert.Equal(expectedLabelText, actualLabelText);
             }
         }
     }
---
+++ CSSelenium/DemoHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+
+namespace CSSelenium
+{
+    /// <summary>
+    /// This class is created to pause the execution (for debugging purpose), if needed.
+    /// Please NOTE that this is just for debugging and is not recommended to use in actual tests.
+    /// </summary>
+    public class DemoHelper
+    {
+        public static void Pause(int sleepTime = 2000)
+        {
+            Thread.Sleep(sleepTime);
+        }
+    }
+}
